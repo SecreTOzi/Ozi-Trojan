e69186622d8626e94b9598669eb9678f76a0034f
Mr.SecreT0zi <67915486+SecreTOzi@users.noreply.github.com>
2020-07-19 20:06:14 +0300

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,54 @@
+#   _   _   ___  _____  _   _____________ _____ _ _     ___   _   _ 
+#  | | | | / _ \/  __ \| | / /_   _| ___ \  _  | | |  / _ \ | \ | |
+#  | |_| |/ /_\ \ /  \/| |/ /  | | | |_/ / | | | | | / /_\ \|  \| |
+#  |  _  ||  _  | |    |    \  | | |    /| | | | | | |  _  || . ` |
+#  | | | || | | | \__/\| |\  \ | | | |\ \\ \_/ / | | | | | || |\  |
+#  \_| |_/\_| |_/\____/\_| \_/ \_/ \_| \_|\___/ / /  \_| |_/\_| \_/  
+#
+#                   ~ Tools For Hacking by Mr. SecreTOzi ~
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/Ozi-Trojan/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/SecreTOzi
+
+clear
+
+mkdir SecreTOzi
+
+clear
+
+cd SecreTOzi
+
+clear
+
+git clone https://github.com/SecreTOzi/Ozi-Trojan
+
+clear
+
+cd Ozi-Trojan
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
